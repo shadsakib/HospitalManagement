6ab4e38435e78cc88ca0870358f6f275bcd057ea
Shadman Sakib <shadmans1@outlook.com>
2019-03-25 23:44:17 +0600

    New Views Added.

--- HospitalManagement/Controllers/HomeController.cs
+++ HospitalManagement/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.WebPages.Html;
 
 namespace HospitalManagement.Controllers
 {
@@ -14,17 +15,125 @@ namespace HospitalManagement.Controllers
             return View();
         }
 
+        [HttpGet]
+        public ActionResult Register()
+        {
+            if(Session["UserId"] == null)
+            {
+                return View();
+            }
+            else
+            {
+                return View("LoggedIn");
+            }
+            
+        }
+
+        [HttpPost]
+        public ActionResult Register(FormCollection collection)
+        {
+            bool filledUp = true;
+            foreach (string key in collection.AllKeys)
+            {
+                if (key.StartsWith("_")) continue;
+                Response.Write("Key = " + key + " , ");
+                Response.Write("Value = " + collection[key]);
+                Response.Write("<br/>");
+                if (collection[key] == "") filledUp = false;
+            }
+
+            if (filledUp) {
+
+                Patient p = new Patient
+                {
+                    PatientName = collection["PatientName"],
+                    BloodGroup = collection["BloodGroup"],
+                    ContactNo = collection["ContactNo"],
+                    Gender = collection["Gender"],
+                    PatientAddress = collection["PatientAddress"],
+                    Username = collection["Username"],
+                    Password = collection["Password"]
+                };
+
+                HospitalContext hospitalContext = new HospitalContext();
+                hospitalContext.Patients.Add(p);
+                hospitalContext.SaveChanges();
+
+                Session["UserId"] = p.PatientId.ToString();
+                Session["Username"] = p.PatientName.ToString();
+                return View("Loggedin");
+            }
+
+            return View();
+        }
+
+
+        public ActionResult Login()
+        {
+            if (Session["UserId"] == null)
+            {
+                return View();
+            }
+            else
+            {
+                return View("LoggedIn");
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Login(FormCollection collection)
+        {
+            if (Session["UserId"] == null)
+            {
+                HospitalContext db = new HospitalContext();
+
+                string username = collection["Username"];
+                string password = collection["Password"];
+
+                Patient p = null;
+                try
+                {
+                    p = db.Patients.Single(u => u.Username == username && u.Password == password);
+                }
+                catch (Exception ex) { }
+
+                if (p != null)
+                {
+                    Session["UserId"] = p.PatientId.ToString();
+                    Session["Username"] = p.PatientName.ToString();
+                    return View("Loggedin");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Incorrect username or password");
+                    return View();
+                }
+            }
+            else
+            {
+                return View("LoggedIn");
+            }
+                            
+        }
+
+        public ActionResult LoggedIn()
+        {
+            if(Session["UserId"] != null)
+            {
+                return View();
+            }
+            else
+            {
+                RedirectToAction("Login");
+                return View("Login");
+            }
+     
+        }
+
         public ActionResult Doctor()
         {
             return View();
         }
-
-        [HttpGet]
-        public ActionResult Register()
-        {
-            return View();
-        }
-
         public ActionResult Patients()
         {
             HospitalContext patientContext = new HospitalContext();
@@ -65,38 +174,14 @@ namespace HospitalManagement.Controllers
         {
             return View();
         }
-        
-        public ActionResult Registration()
+
+        public ActionResult Profile()
         {
             return View();
         }
 
-        [HttpPost]
-        public ActionResult Register(FormCollection collection)
+        public ActionResult Test()
         {
-            foreach(string key in collection.AllKeys)
-            {
-                if (key.StartsWith("_") )continue;
-                Response.Write("Key = " + key + " , ");
-                Response.Write("Value = " +collection[key]);
-                Response.Write("<br/>");
-            }
-
-            Patient p = new Patient
-            {
-                PatientName = collection["PatientName"],
-                BloodGroup = collection["BloodGroup"],
-                ContactNo = collection["ContactNo"],
-                Gender = collection["Gender"],         
-                PatientAddress = collection["PatientAddress"],
-                Username = collection["Username"],
-                Password = collection["Password"]
-            };
-
-            HospitalContext hospitalContext = new HospitalContext();
-            //hospitalContext.Patients.Add(p);
-            //hospitalContext.SaveChanges();
-
             return View();
         }
     }
--- HospitalManagement/Models/Patient.cs
+++ HospitalManagement/Models/Patient.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -10,12 +11,21 @@ namespace HospitalManagement.Models
     public class Patient
     {
         public int PatientId{ get; set; }
+
+        [Required(ErrorMessage = "This field is required")]
         public string PatientName { get; set; }
+        [Required(ErrorMessage = "This field is required")]
         public string Username { get; set; }
+        [Required(ErrorMessage = "This field is required")]
         public string Password { get; set; }
+        [Required(ErrorMessage ="This field is required")]
         public string Gender { get; set; }
+        [Required(ErrorMessage = "This field is required")]
         public string PatientAddress { get; set; }
+        [Required(ErrorMessage = "This field is required")]
         public string ContactNo { get; set; }
+        [Required(ErrorMessage = "This field is required")]
         public string BloodGroup { get; set; }
-}
+    }
+
 }
